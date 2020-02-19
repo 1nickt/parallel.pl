f0f7c4f09d98e76c8257c7035eccd145eb098df7
Nick Tonkin <nick@nicktonkin.net>
2020-02-18 22:05:29 -0500

    Tersify email subject

--- lib/MDA/ModuleMetaData.pm
+++ lib/MDA/ModuleMetaData.pm
@@ -146,8 +146,7 @@ method _send_version_announcement ($updated) {
 
     return unless $current_data;
 
-    my $subject = 'CPAN release announcement: ';
-    $subject .= join(' and ', map { sprintf('%s %s', $_, $current_data->{$_}{version}) } sort keys %$updated );
+    my $subject = join(' and ', map { sprintf('%s %s', $_, $current_data->{$_}{version}) } sort keys %$updated );
 
     my @template_data = map { { module => $_, %{$current_data->{$_}} } } sort keys %$updated;
 
